441212e535765afe8d8f2b236458bda6ecf4e0d6
Vishal kumar <vishalk80269@gmail.com>
2025-06-18 23:54:54 +0530

    testing

--- utils/features.js
+++ utils/features.js
@@ -31,7 +31,7 @@ const sendToken = (res, user, code, message) => {
     message,
   });
 };
-
+// vishal kumar
 const emitEvent = (req, event, users, data) => {
   const io = req.app.get("io");
   const usersSocket = getSockets(users);
