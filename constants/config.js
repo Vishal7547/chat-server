0f15c8acd0d3c19814cb51de3a881f83776b0bcd
Vishal kumar <vishalk80269@gmail.com>
2025-06-19 13:58:06 +0530

    fix small bugs

--- constants/config.js
+++ constants/config.js
@@ -4,8 +4,11 @@ const corsOptions = {
     "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  exposedHeaders: ["set-cookie"],
+  optionsSuccessStatus: 204,
 };
 
 const CHAT_TOKEN = "chat-token";
